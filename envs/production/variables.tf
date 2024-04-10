7371239d90efcb9fbd171e3895ad4324094eb5f1
okmtdev <okmtdev@gmail.com>
2024-04-11 00:09:18 +0900

    update variables

--- envs/production/variables.tf
+++ envs/production/variables.tf
@@ -2,6 +2,14 @@ variable "environment" {
   default = "prod"
 }
 
+variable "test_wordpress_db_username" {
+  default = "username"
+}
+
+variable "test_wordpress_db_password" {
+  default = "password"
+}
+
 variable "wordpress_db_username" {
   default = "username"
 }
