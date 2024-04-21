ca0d5ef5cbabde516ec35319dc2c2af4dee991d8
okmtdev <okmtdev@gmail.com>
2024-04-21 23:45:12 +0900

    minodr update

---
+++ envs/sandbox/README.md
@@ -0,0 +1,3 @@
+```
+ssh -i ~/.ssh/sandbox_aws_key ec2-user@ec2-52-197-187-210.ap-northeast-1.compute.amazonaws.com
+```
---
+++ modules/ecr/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  description = "The environment in which the ECR repository will be used."
+  type        = string
+}
+
+variable "repository" {
+  description = "The name of the ECR repository."
+  type        = string
+}
