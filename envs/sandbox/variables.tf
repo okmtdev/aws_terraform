da32e071a9eb9c2a8d5e5015a4e71a2f53f54d1f
okmtdev <okmtdev@gmail.com>
2024-04-23 02:23:30 +0900

    add dynamodb

--- envs/sandbox/variables.tf
+++ envs/sandbox/variables.tf
@@ -5,3 +5,21 @@ variable "environment" {
 variable "zone_id" {
   default = "Z06637531YBCMH55FF8KU" # okmtdev.com
 }
+
+variable "attributes" {
+  description = "A list of attributes to be used by the DynamoDB table"
+  type = list(object({
+    name = string
+    type = string
+  }))
+  default = [
+    {
+      name = "UserId",
+      type = "S"
+    },
+    {
+      name = "GameTitle",
+      type = "S"
+    }
+  ]
+}
